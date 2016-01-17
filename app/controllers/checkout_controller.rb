b62edc3cfbf2750c54b7ba2c15ccbc5d0b45d05f
Darrel Ong <darrelalvin@yahoo.com>
2016-01-17 03:47:26 +0000

    added html image pixel

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -17,21 +17,28 @@ class CheckoutController < ApplicationController
 	products = CGI.escape(products)
 	amount = CGI.escape(amount)
 
-	postback_url = "http://ipsyresearch.go2cloud.org/aff_lsr?transaction_id=#{transaction_id}&user_id=#{user_id}&adv_sub=#{secret}&adv_sub2=#{products}&amount=#{amount}"
-  	puts "[DEBUG] Postback URL: #{postback_url}"
+	if transaction_id != ""
+		postback_url = "http://ipsyresearch.go2cloud.org/aff_lsr?transaction_id=#{transaction_id}&user_id=#{user_id}&adv_sub=#{secret}&adv_sub2=#{products}&amount=#{amount}"
+		puts "[DEBUG] Postback URL: #{postback_url}"
 
-	# sending the conversion link
-	require 'nokogiri'
-	#require 'open-uri'
-	require 'open_uri_redirections'
-	doc = Nokogiri::HTML(open(postback_url, :allow_redirections => :all))
-	puts "[DEBUG] #{doc}"
-	if doc.at('body').text != "success=true;"
-		puts "ERROR: problem in sending conversion_url"
-		return
+		# sending the conversion link
+		require 'nokogiri'
+		#require 'open-uri'
+		require 'open_uri_redirections'
+		doc = Nokogiri::HTML(open(postback_url, :allow_redirections => :all))
+		puts "[DEBUG] #{doc}"
+		if doc.at('body').text != "success=true;"
+			puts "ERROR: problem in sending conversion_url"
+			return
+		end
+  		redirect_to "http://localhost:3000" # redirects back to ipsyploos dummy site
 	end
+
+	if transaction_id == ""
+		puts "No transaction ID is passed. See html page"
+	end
+
 	puts "[DEBUG] TIME LAPSED: #{Time.now-t0} seconds"
 	
-  	redirect_to "http://localhost:3000" # redirects back to ipsyploos dummy site
   end
 end
